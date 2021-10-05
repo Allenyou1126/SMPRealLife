c495ef25efa5ec55803eefccfbcab00d40359faf
Allenyou1126 <allenyou1126@gmail.com>
2021-10-05 14:47:39 +0800

    修改主类名称

--- src/main/java/wang/allenyou/SMPRealLife.java
+++ src/main/java/wang/allenyou/SMPRealLife.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 // TODO 添加生命数系统
 // TODO 添加生命数为零则变成旁观者设定
 
-public class SMPReallife extends JavaPlugin {
+public class SMPRealLife extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
