923929ed17c06c335123e3ecf76b64c4523ea2b0
Allenyou1126 <allenyou1126@gmail.com>
2021-10-05 14:04:48 +0800

    初始化IDEA项目，maven添加Spigot1.17.1依赖

---
+++ src/main/java/wang/allenyou/SMPReallife.java
@@ -0,0 +1,15 @@
+package wang.allenyou;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SMPReallife extends JavaPlugin {
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
